7c29a0d0306923edf7aac46c62e199b59826ee1b
Harshit Dubey <hdlikes8@gmail.com>
2024-06-13 02:27:59 +0530

    Feature : Laugh BOO and Abuse

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -5,6 +5,8 @@ import { VscChromeClose } from "react-icons/vsc";
 import "./Home.css";
 import Spinner from "../../components/Spinner";
 import resultSound from "./../../assets/fart-sound.mp3";
+import laughSound from "./../../assets/laugh.mp3";
+import booSound from "./../../assets/boo.mp3";
 
 const Home = () => {
   const initialState = {
@@ -16,6 +18,10 @@ const Home = () => {
   const [showResults, setShowResults] = useState(false);
   const [loading, setLoading] = useState(false);
   const resultAudio = new Audio(resultSound);
+  const laughAudio = new Audio(laughSound);
+  const booAudio = new Audio(booSound);
+  const [abuse, setAbuse] = useState(false);
+  const [abuseMessage, setAbuseMessage] = useState("");
 
   const onHandleChange = (index, e) => {
     const { name, value } = e.target;
@@ -56,7 +62,31 @@ const Home = () => {
     setGuyFarted("");
   };
 
+  const handleLaugh = () => {
+    laughAudio.play();
+  };
+
+  const handleBoo = () => {
+    booAudio.play();
+  };
+
   const disableSubmit = data.every((inputField) => inputField.name === "");
+
+  const handleAbuse = () => {
+    const abuseMessages = [
+      "Whoa, what crawled up there and died?",
+      "That was so foul, even the flies left the room!",
+      "Seriously bro, that's weaponized!",
+
+      "Did you eat a can of beans for breakfast?",
+
+      "Are you trying to clear the room or summon ghosts?",
+      "Next time, warn us before you launch a stink bomb!",
+    ];
+    const randomIndex = Math.floor(Math.random() * abuseMessages.length);
+    setAbuseMessage(abuseMessages[randomIndex]);
+    setAbuse(true);
+  };
 
   return (
     <div className="home">
@@ -107,7 +137,26 @@ const Home = () => {
           <h3 className="home-heading">Fellow has farted!</h3>
 
           <h2 className="winner-name">{guyFarted}</h2>
-          <p className="home-sub-heading">Hey dude, gross! Bro, seriously!</p>
+
+          <p className="home-sub-heading">{abuseMessage}</p>
+
+          <div className="result-buttons">
+            <button
+              className="submit-button action-button"
+              onClick={handleLaugh}
+            >
+              Laugh at him
+            </button>
+            <button className="submit-button action-button" onClick={handleBoo}>
+              Boo Him/Her
+            </button>
+            <button
+              className="submit-button action-button"
+              onClick={handleAbuse}
+            >
+              {abuse ? "Retry Abusing" : "Abuse"}
+            </button>
+          </div>
           <button className="submit-button back-button " onClick={goBack}>
             Go Back
           </button>
